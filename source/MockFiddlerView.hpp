3fc04f4a6de2ebaf5752510a6529db68c47c97f9
Finn <finn.wundram@stud.hs-emden-leer.de>
2022-04-17 16:53:56 +0200

    display wird nicht mehr als statisch deklariert

--- source/MockFiddlerView.hpp
+++ source/MockFiddlerView.hpp
@@ -7,7 +7,7 @@
 
 class MockFiddlerView{
     public:
-        virtual static std::string display(Fiddler target) = 0;
+        virtual std::string display(Fiddler target) = 0;
 };
 
 using Mfd_View = std::shared_ptr<MockFiddlerView>;
