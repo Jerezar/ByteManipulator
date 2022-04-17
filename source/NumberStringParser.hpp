92dcc591e511aba64479a66e36861690bc61aa9d
Finn <finn.wundram@stud.hs-emden-leer.de>
2022-04-17 16:27:10 +0200

    Fehlendes Semicolon berichtigt

--- source/NumberStringParser.hpp
+++ source/NumberStringParser.hpp
@@ -8,7 +8,7 @@
 class NumberStringParser{
     public:
         virtual int getValueFromString(std::string string_rep) = 0;
-}
+};
 
 using ValueParser = std::shared_ptr<NumberStringParser>;
 #endif
