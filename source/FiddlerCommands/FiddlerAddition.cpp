a826eda1473fb6b33ab9dbbdd3a2482af652d5e3
Finn <finn.wundram@stud.hs-emden-leer.de>
2022-04-17 15:31:06 +0200

    Addiert jetzt eingegebene Zahlen

--- source/FiddlerCommands/FiddlerAddition.cpp
+++ source/FiddlerCommands/FiddlerAddition.cpp
@@ -1,7 +1,11 @@
 #include "FiddlerAddition.hpp"
+#include <exception>
 
-std::string FiddlerAddition::execute(std::vector<std::string> args){
-    target->byte += 5;
-    
-    return std::to_string((int) target->byte);
+std::string FiddlerAddition::execute(std::vector<std::string> args) {
+    if(args.size() == 1){
+        throw std::exception();
+        return std::to_string( (int) target->byte);
+    }
+    target->byte += std::stoi(args.at(1));
+    return std::to_string( (int) target->byte);
 }
