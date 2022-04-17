fd96b51c42a36607e8e18798b7d358bf6ca512a6
Finn <finn.wundram@stud.hs-emden-leer.de>
2022-04-17 19:08:43 +0200

    Byte wird als bitset dargestellt

--- source/FiddlerFullView.cpp
+++ source/FiddlerFullView.cpp
@@ -1,13 +1,15 @@
 #include "FiddlerFullView.hpp"
 
+#include <bitset>
+
 FiddlerFullView::FiddlerFullView(){
 
 }
 
 std::string FiddlerFullView::display(Fiddler target){
     std::string result("");
-    result += "Byte\tCarry\tZero\tParity\n";
-    result += std::to_string(target->byte) + "\t" + std::to_string(target->carry) + "\t" + std::to_string(target->zero) + "\t" + std::to_string(target->parity) + "\n";
+    result += "Byte\t\tCarry\tZero\tParity\n";
+    result += std::bitset<8>(target->byte).to_string() + "\t" + std::to_string(target->carry) + "\t" + std::to_string(target->zero) + "\t" + std::to_string(target->parity) + "\n";
     
     return result;
 }
