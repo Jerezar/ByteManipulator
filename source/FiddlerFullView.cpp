fac1d4ab9d78accf626242039a85772831dbea59
Finn <finn.wundram@stud.hs-emden-leer.de>
2022-04-17 20:14:58 +0200

    Zeit jetzt Rest an

--- source/FiddlerFullView.cpp
+++ source/FiddlerFullView.cpp
@@ -8,8 +8,8 @@ FiddlerFullView::FiddlerFullView(){
 
 std::string FiddlerFullView::display(Fiddler target){
     std::string result("");
-    result += "Byte\t\tCarry\tZero\tParity\n";
-    result += std::bitset<8>(target->byte).to_string() + "\t" + std::to_string(target->carry) + "\t" + std::to_string(target->zero) + "\t" + std::to_string(target->parity) + "\n";
+    result += "Byte\t\tRest\tCarry\tZero\tParity\n";
+    result += std::bitset<8>(target->byte).to_string() + "\t" + std::to_string(target->rest) + "\t" + std::to_string(target->carry) + "\t" + std::to_string(target->zero) + "\t" + std::to_string(target->parity) + "\n";
     
     return result;
 }
