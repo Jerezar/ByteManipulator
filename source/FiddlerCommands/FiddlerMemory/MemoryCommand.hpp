a9f890b227bede01f9f4685d7ba0f53d5b42b5de
Finn <finn.wundram@ewe.net>
2022-04-22 13:05:41 +0200

    MemoryCommand, virtual subclass von InstructionWrapper

---
+++ source/FiddlerCommands/FiddlerMemory/MemoryCommand.hpp
@@ -0,0 +1,19 @@
+#ifndef MEMORYCOMMAND_HPP
+#define MEMORYCOMMAND_HPP
+
+#include "InstructionWrapper.hpp"
+#include "TypedMemory.hpp"
+#include <memory>
+
+
+class MemoryCommand: virtual public InstructionWrapper{
+    protected:
+        std::shared_ptr<TypedMemory> mem;
+    public:
+        MemoryCommand(std::shared_ptr<TypedMemory> _mem)(){
+            mem = _mem;
+        }
+};
+
+#endif
+
