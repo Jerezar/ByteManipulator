be161646471c454dd2bb77f0902a10d02b242ce6
Finn <finn.wundram@ewe.net>
2022-05-28 03:13:55 +0200

    StringParser sollten aussagen können, ob der String interpretiert werden kann

--- source/NumberStringParser.hpp
+++ source/NumberStringParser.hpp
@@ -8,6 +8,7 @@
 class NumberStringParser{
     public:
         virtual int getValueFromString(std::string string_rep) = 0;
+        virtual bool canParse(std::string string_rep) = 0;
 };
 
 using ValueParser = std::shared_ptr<NumberStringParser>;
