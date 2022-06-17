5e235dc343fd297a9478942a08b640a74e18a66f
Finn <finn.wundram@ewe.net>
2022-06-17 03:06:18 +0200

    Methode die aus 32int einen entsprechenden utf8 string machen

--- source/string_utils.cpp
+++ source/string_utils.cpp
@@ -40,6 +40,28 @@ namespace fw_byte_manip{
             return result;
         }
         
+        std::string utf8_string_from_int32(int c){
+            std::string result;
+            
+            if(c & 0xFF000000){
+                result.push_back((char) (c >> (3* 8) ));
+                result.push_back((char) (c >> (2* 8) ));
+                result.push_back((char) (c >> (1* 8) ));
+                result.push_back((char) (c >> (0* 8) ));
+            } else if(c & 0xFF0000){
+                result.push_back((char) (c >> (2* 8) ));
+                result.push_back((char) (c >> (1* 8) ));
+                result.push_back((char) (c >> (0* 8) ));
+            } else if(c & 0xFF00){
+                result.push_back((char) (c >> (1* 8) ));
+                result.push_back((char) (c >> (0* 8) ));
+            } else {
+                result.push_back((char) (c >> (0* 8) ));
+            }
+            
+            return result;
+        }
+        
         std::string ansi_8b_col_text(std::string text, uint8_t value){
             return "\u001b[38;5;" + std::to_string(value) + "m" + text + "\u001b[0m"; 
         }
--- source/string_utils.hpp
+++ source/string_utils.hpp
@@ -7,6 +7,7 @@ namespace fw_byte_manip{
     namespace string_utils{
         std::vector< std::string > split(std::string input, std::string delim);
         int codepoint_to_utf8( uint32_t c );
+        std::string utf8_string_from_int32(int c);
         std::string ansi_8b_col_text(std::string text, uint8_t value);
         std::string ansi_8b_col_bg(std::string text, uint8_t value);
     }
