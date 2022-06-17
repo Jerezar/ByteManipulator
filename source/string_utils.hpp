c571b9005622dc400c9545758f989dd7c4b414d8
Finn <finn.wundram@ewe.net>
2022-06-17 02:38:19 +0200

    Methode, um unicode code-point in seiner utf8 kodierung in einem 32bit integer zu liefern

--- source/string_utils.hpp
+++ source/string_utils.hpp
@@ -6,7 +6,7 @@
 namespace fw_byte_manip{
     namespace string_utils{
         std::vector< std::string > split(std::string input, std::string delim);
-        /*std::string iso_8859_1_escape(uint8_t value);*/
+        int codepoint_to_utf8( uint32_t c );
         std::string ansi_8b_col_text(std::string text, uint8_t value);
         std::string ansi_8b_col_bg(std::string text, uint8_t value);
     }
